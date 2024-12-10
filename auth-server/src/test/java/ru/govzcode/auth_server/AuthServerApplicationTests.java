644c2f36d3d761eb8e34d4337df971df6b305abc
rozoomcool <rozoom.um@gmail.com>
2024-12-10 08:03:13 +0300

    add auth server

---
+++ auth-server/src/test/java/ru/govzcode/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.govzcode.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
