de520a407c902709b02a01e55710855a82786811
rozoomcool <rozoom.um@gmail.com>
2024-12-13 11:40:25 +0300

    .

---
+++ config/src/test/java/ru/govzcode/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.govzcode.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
